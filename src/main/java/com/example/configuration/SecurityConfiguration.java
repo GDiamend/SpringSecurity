e5f453a646fadecb29510129932c35da0b521c02
GDiamend <Gdiamend@gmail.com>
2023-07-24 20:29:42 -0300

    Added configuration
    Added configurations:
    .authorizeHttpRequests
    .formLogin
    .sessionManagement
    (also necessary methods :
    AuthenticationSuccessHandler
    SessionRegistry)

--- src/main/java/com/example/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/configuration/SecurityConfiguration.java
@@ -4,22 +4,62 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+	// First way to write the configuration's option
+	/*
+	 * @Bean 
+	 * public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception { 
+	 * return httpSecurity 
+	 * .authorizeHttpRequests()
+	 * .requestMatchers("v1/index2").permitAll() 
+	 * .anyRequest().authenticated()
+	 * .and() 
+	 * .formLogin().permitAll() 
+	 * .and() 
+	 * .build(); }
+	 */
 	
+	// Second way to write the configuration's option
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity
-				.authorizeHttpRequests()
-					.requestMatchers("v1/index2").permitAll()
-					.anyRequest().authenticated()
-				.and()
-					.formLogin().permitAll()
-				.and()
+				.authorizeHttpRequests(auth -> {
+					auth.requestMatchers("/v1/index2").permitAll();
+					auth.anyRequest().authenticated();
+				})
+				.formLogin(auth -> {
+					auth.successHandler(succesHandler());
+					auth.permitAll();
+				})				
+				.sessionManagement(auth -> {
+					auth.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+					auth.invalidSessionUrl("/login");
+					auth.maximumSessions(1)
+						.sessionRegistry(sessionRegistry())
+						.expiredUrl("/login");
+					auth.sessionFixation()
+						.migrateSession();
+				})
 				.build();
 	}
-
+	
+	public AuthenticationSuccessHandler succesHandler() {
+		return ((request, response, authentication) -> {
+			response.sendRedirect("/v1/session");
+		});
+	}
+	
+	@Bean
+	public SessionRegistry sessionRegistry() {
+		return new SessionRegistryImpl();
+	}
 }
--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +17,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1")
 public class CustomerController {
 
+	@Autowired
+	private SessionRegistry sessionRegistry;
+
 	@GetMapping("/index")
 	public String index() {
 		return "Secured index";
 	}
-	
+
 	@GetMapping("/index2")
 	public String index2() {
 		return "Unsecured index";
+	}
+
+	@GetMapping("/session")
+	public ResponseEntity<?> getDetailsSession() {
+		String sessionId = "";
+		User userObject = null;
+
+		List<Object> sessions = this.sessionRegistry.getAllPrincipals();
+
+		for (Object session : sessions) {
+			if (session instanceof User) {
+				userObject = (User) session;
+			}
+
+			List<SessionInformation> sessionInformations = this.sessionRegistry.getAllSessions(session, false);
+			
+			for(SessionInformation sessionInformation : sessionInformations) {
+				sessionId = sessionInformation.getSessionId();
+			}
+		}
+		
+		Map<String, Object> response = new HashMap<>();
+		response.put("response", "Hello galaxy");
+		response.put("sessionId", sessionId);
+		response.put("sessionUser", userObject);
+		
+		return ResponseEntity.ok(response);
 	}
 }
